20fa999abde2bb106423f99eb24f169c2bcaa1f3
Ramansh Saxena <ramansh8055@gmail.com>
2025-01-27 15:56:58 +0530

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+const SWATCHES = [
+    "#000000",  // black
+"#ffffff",  // white
+"#ee3333",  // red
+"#e64980",  // pink
+"#3333ee",  // dark blue
+"#40c057",  // green
+"#fab005",  // yellow
+"#fd7e14",  // orange
+"#8e44ad",  // violet
+"#f39c12",  // gold
+
+"#16a085",  // sea green
+
+];
+  
+export { SWATCHES };
